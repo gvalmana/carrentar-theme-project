1b92d6eb62e68d309b05ec38fcd0b7a694ee5d89
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-03 10:15:57 -0500

    added carousel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('¡Hola, mundo!');
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtén todos los elementos con la clase "card-car"
+    var cardCarElements = document.querySelectorAll(".card-car");
+
+    // Itera sobre cada elemento y agrega los listeners
+    cardCarElements.forEach(function(element) {
+        // Agrega el listener para el evento mouseenter
+        element.addEventListener("mouseenter", function() {
+            // Agrega las clases "z-3" y "shadow" al hacer hover
+            element.classList.add("z-1", "shadow-sm");
+        });
+
+        // Agrega el listener para el evento mouseleave
+        element.addEventListener("mouseleave", function() {
+            // Elimina las clases "z-3" y "shadow" al salir del hover
+            element.classList.remove("z-1", "shadow-sm");
+        });
+    });
+});
